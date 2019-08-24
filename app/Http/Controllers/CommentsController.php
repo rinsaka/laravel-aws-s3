333eb377fac9b9c7465180ead38f53f79a6d5c44
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-08-24 12:57:20 +0900

    非公開ファイルに署名を付けて表示

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -4,20 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\App;
 
 class CommentsController extends Controller
 {
   public function index()
   {
+    // 共有ファイルは単純
     $path001 = Storage::disk('s3')->url('001.png');
     // $path002 = Storage::disk('s3')->url('002.png');
+    // 共有ファイルに有効期限を付けてみる
     $path002 = Storage::disk('s3')->temporaryUrl('002.png', now()->addMinutes(5));
-    // $path003 = Storage::disk('s3')->temporaryUrl('iam001.png', now()->addMinutes(5));
-    $path003 = Storage::disk('s3')->url('iam001.png');
-    // dd($path);
+    // 非公開ファイル
+    $path003 = Storage::disk('s3')->url('iam01.png');
+    // 非公開ファイルに有効期限を付けてみる
+    $path004 = Storage::disk('s3')->temporaryUrl('iam01.png', now()->addMinutes(5));
+
+    // この書き方でも良い
+    $s3Client= App::make('aws')->createClient('s3');
+    $cmd = $s3Client->getCommand('GetObject', [
+      'Bucket' => env('AWS_BUCKET'),
+      'Key' => 'iam01.png',
+    ]);
+    $request = $s3Client->createPresignedRequest($cmd, '+1 minutes');
+    $path005 = (string) $request->getUri();
+
     return view('comments.index')
       ->with('path001', $path001)
       ->with('path002', $path002)
-      ->with('path003', $path003);
+      ->with('path003', $path003)
+      ->with('path004', $path004)
+      ->with('path005', $path005);
   }
 }
--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -2,18 +2,45 @@
 <html lang="ja">
 <head>
 <meta charset="utf-8">
-<title>comments</title>
+<title>S3 からファイルを取得する</title>
 </head>
 <body>
-  <h1>Comments</h1>
+  <h1>S3 からファイルを取得する</h1>
+  <h3>共有ファイルは簡単</h3>
   <p>
+    {{$path001}}<br>
     <img src="{{$path001}}">
   </p>
+  <h3>共有ファイルに有効期限を付けることもできる</h3>
   <p>
+    再読み込みをすると上に比べてちょっと時間がかかる<br>
+    読込みのたびに X-Amz-Date と X-Amz-Signature が更新される．<br>
+    {{$path002}}<br>
     <img src="{{$path002}}">
   </p>
+  <h3>非公開ファイル</h3>
+
   <p>
+    ファイルの URL が正しくても表示できない（これでよい）<br>
+    {{$path003}}<br>
     <img src="{{$path003}}">
+  </p>
+
+  <h3>非公開ファイルに有効期限を設定</h3>
+
+  <p>
+    ファイルの URL に有効期限だけでなく署名も付くので表示できた<br>
+    {{$path004}}<br>
+    <img src="{{$path004}}">
+  </p>
+
+  <h3>別の書き方</h3>
+
+  <p>
+    別の書き方でも良い<br>
+    いずれにせよ，URLの有効期限があるので，有効期限が切れると別のブラウザでは表示できなくなる<br>
+    {{$path005}}<br>
+    <img src="{{$path005}}">
   </p>
 </body>
 </html>
